ea2b642f2fc23dc1aa564e83745e155f0121c8d3
Robert Sorić <robert.soric@gmail.com>
2025-04-09 09:03:42 +0200

    Added slider-potentiometer

---
+++ soldered-documentation/docs/sensors/slider-potentiometer/arduino_library/slider-potentiometer-arduino-4.md
@@ -0,0 +1,24 @@
+---
+slug: /slider-potentiometer/troubleshooting
+title: Troubleshooting
+id: slider-potentiometer-arduino-4
+hide_title: False
+pagination_next: null
+---
+
+This page contains some tips if you are having problems using this product.
+
+<ExpandableSection title="My sensor won't initialize!">
+
+#### Check wiring
+Ensure that your Qwiic cable is properly connected and in good condition. Try using the same cable with another easyC-compatible device to verify that it works. If the issue persists, swap it out for a different cable to rule out any possible damage or defects.
+
+#### Check I2C pins
+If you are connecting the sensor using standard I2C pins on your microcontroller, double-check that you are using the correct ones. Different microcontrollers have designated I2C pins that may not always be labeled the same way. Refer to your microcontroller's documentation to confirm the correct pin assignments.
+
+#### Scan for I2C devices
+Run an [**I2C scanner sketch**](https://github.com/SolderedElectronics/Soldered-Hacky-Codes/tree/main/I2C_Scanner) on your microcontroller to check if the sensor is detected. If the scanner does not find any devices, there might be a wiring issue, incorrect pull-up resistors, or a problem with the microcontroller’s I2C bus.
+
+</ExpandableSection>
+
+<InfoBox>If you haven't found the answer to your question, please **contact us** via [**this**](https://soldered.com/contact/) link.</InfoBox>
--- soldered-documentation/sidebars.js
+++ soldered-documentation/sidebars.js
@@ -329,6 +329,46 @@ const sidebars = {
         },
         {
           type: 'category',
+          label: 'Slider potentiometer',
+          items: [
+            {
+              type: 'doc',
+              id: 'sensors/slider-potentiometer/slider-potentiometer-overview',
+            },
+            {
+              type: 'doc',
+              id: 'sensors/slider-potentiometer/slider-potentiometer-hardware',
+            },
+            {
+              type: 'doc',
+              id: 'sensors/slider-potentiometer/slider-potentiometer-how-it-works',
+            },
+            {
+              type: 'category',
+              label: 'Arduino library',
+              items: [
+                {
+                  type: 'doc',
+                  id: 'sensors/slider-potentiometer/arduino_library/slider-potentiometer-arduino-1',
+                },
+                {
+                  type: 'doc',
+                  id: 'sensors/slider-potentiometer/arduino_library/slider-potentiometer-arduino-2',
+                },
+                {
+                  type: 'doc',
+                  id: 'sensors/slider-potentiometer/arduino_library/slider-potentiometer-arduino-3',
+                },
+                {
+                  type: 'doc',
+                  id: 'sensors/slider-potentiometer/arduino_library/slider-potentiometer-arduino-4',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'category',
           label: 'PIR Movement sensor',
           items: [
             {
