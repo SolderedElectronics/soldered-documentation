21761440bd63fc0a00a2bb396ed9631cf80f05ca
Robert Sorić <robert.soric@gmail.com>
2026-03-10 12:52:58 +0100

    Added beginner kits files

--- soldered-documentation/docusaurus.config.js
+++ soldered-documentation/docusaurus.config.js
@@ -17,10 +17,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://soldered.com',
+  url: 'https://docs.soldered.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
