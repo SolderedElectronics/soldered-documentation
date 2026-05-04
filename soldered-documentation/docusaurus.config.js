7aca07396a12c47565d92edc37c3b3e06f2a92f2
Štefan Granatir <116319250+stefangranatir@users.noreply.github.com>
2026-05-04 10:44:20 +0200

    Added new links for battery docs and fixed trailing slash error for document links

--- soldered-documentation/docusaurus.config.js
+++ soldered-documentation/docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  trailingSlash: true,
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
